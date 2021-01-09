01eaae78e552fec21fb14d6be49b5ee5648041b7
Keniran <naani.navis@gmail.com>
2021-01-09 18:34:41 -0500

    created classes and added shuffling and dealing

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    private final List<Card> hand;
+    private int chips;
+
+    public Player() {
+        this.hand = new ArrayList<>();
+        this.chips = 0;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public int getChips() {
+        return chips;
+    }
+
+    public void deal(Card card) {
+        hand.add(card);
+        sortHand();
+    }
+
+    private void sortHand() {
+        hand.sort((a, b) -> {
+            if (Card.getOrderedRank(a.getRank()) == Card.getOrderedRank(b.getRank())) {
+                return Card.getOrderedSuit(a.getSuit()) - Card.getOrderedSuit(b.getSuit());
+            }
+
+            return Card.getOrderedRank(a.getRank()) - Card.getOrderedRank(b.getRank());
+        });
+    }
 }
--- src/Poker.java
+++ src/Poker.java
@@ -1,2 +1,59 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Poker {
+    private final String[] SUITS = { "C", "D", "H", "S" };
+    private final String[] RANKS = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K" };
+
+    private final Player player;
+    private List<Card> deck;
+    private final Scanner in;
+
+    public Poker() {
+        this.player = new Player();
+        this.in = new Scanner(System.in);
+    }
+
+    public void game() {
+        shuffle();
+
+        while (true) {
+            for (int i = 0; i < 5; i++) {
+                player.deal(deck.get(0));
+                deck.remove(0);
+            }
+
+            System.out.print("\nYour hand: [");
+            for (int i = 0; i < player.getHand().size() - 1; i++) {
+                System.out.print(player.getHand().get(i).toString() + ", ");
+            }
+            System.out.println(player.getHand().get(player.getHand().size() - 1).toString() + "]");
+
+            takeTurn();
+
+            break;
+        }
+    }
+
+    public void takeTurn() {
+
+    }
+
+    public void shuffle() {
+        deck = new ArrayList<>(52);
+
+        for (String suit : SUITS) {
+            for (String rank : RANKS) {
+                deck.add(new Card(rank, suit));     // adds 52 cards to the deck (13 ranks, 4 suits)
+            }
+        }
+
+        Collections.shuffle(deck);
+    }
+
+    public static void main(String[] args) {
+        new Poker().game();
+    }
 }
